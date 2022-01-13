9c1aeb778bd0886d28f65ee37cd4aa8e64a66b95
Lei Yang <leiy@csail.mit.edu>
2022-01-13 21:43:18 +0000

    switch to csv files

--- data_to_plot.py
+++ data_to_plot.py
@@ -15,31 +15,25 @@ parser.add_argument('--input',
                     type=str,
                     help="Input JSON data file",
                     default="output.json")
+parser.add_argument('--output',
+                    type=str,
+                    help="Output prefix",
+                    default="")
 
 # Expt parameters
 args = parser.parse_args()
 
-def to_dots(x, y):
-    if len(x) != len(y):
-        print("error")
-        os.exit(0)
-    series = []
-    for i in range(len(x)):
-        o = "({}, {})".format(x[i], y[i])
-        series.append(o)
-    return ' '.join(series)
-
-
 if __name__ == "__main__":
     with open(args.input) as f:
         d = json.load(f)
-        template = """\\addplot [myparula11] coordinates {{
-                {}
-        }};"""
+        # chain growth
         first_chain_switch = d['first_chain_switch']
         for k in d['victims']:
             height = d['victims'][k]['chain_growth']['height']
             timestamp = d['victims'][k]['chain_growth']['timestamp']
             l = len(height)
             x = [(t-first_chain_switch)/1000000.0 for t in timestamp]
-            print(template.format(to_dots(x, height)))
+            with open(args.output+"chain_growth_victim_{}.txt".format(k), 'w') as outfile:
+                outfile.write("time height\n")
+                for i in range(l):
+                    outfile.write("{} {}\n".format(x[i], height[i]))
